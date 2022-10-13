d99bfb7c4879c98032a8d0a8378cab07aabd83f2
Halfmai <halfloligo.h@gmail.com>
2022-10-13 11:52:06 +0800

    formatting

--- tagAutocomplete.js
+++ tagAutocomplete.js
@@ -183,8 +183,8 @@ function insertTextAtCursor(text, tagword) {
     promptTextbox.selectionEnd = promptTextbox.selectionStart;
 
     // Since we've modified a Gradio Textbox component manually, we need to simulate an `input` DOM event to ensure its
-	// internal Svelte data binding remains in sync.
-	promptTextbox.dispatchEvent(new Event("input", { bubbles: true }));
+    // internal Svelte data binding remains in sync.
+    promptTextbox.dispatchEvent(new Event("input", { bubbles: true }));
 
     // Hide results after inserting
     hideResults();
