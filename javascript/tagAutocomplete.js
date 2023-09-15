2622e1b596fce9c3988d1932e0d9a235a9085a43
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2023-09-15 21:12:30 +0700

    Refresh extra: fix python code did not excecuted

--- javascript/tagAutocomplete.js
+++ javascript/tagAutocomplete.js
@@ -1324,12 +1324,15 @@ async function setup() {
         })
     });
     // Listener for internal temp files refresh button
-    gradioApp().querySelector("#refresh_tac_refreshTempFiles")?.addEventListener("click", refreshTacTempFiles);
+    const refreshButton = gradioApp().querySelector("#refresh_tac_refreshTempFiles")
+    refreshButton?.addEventListener("click", refreshTacTempFiles);
 
-    // Also add listener for external network refresh button
-    gradioApp().querySelector("#txt2img_extra_refresh")?.addEventListener("click", refreshTacTempFiles);
-    gradioApp().querySelector("#img2img_extra_refresh")?.addEventListener("click", refreshTacTempFiles);
-
+    // Also add listener for external network refresh button (plus triggering python code)
+    ["#img2img_extra_refresh", "#txt2img_extra_refresh"].forEach(e => {
+        gradioApp().querySelector(e)?.addEventListener("click", ()=>{
+            refreshButton?.click();
+        });
+    })
 
     // Add mutation observer for the model hash text to also allow hash-based blacklist again
     let modelHashText = gradioApp().querySelector("#sd_checkpoint_hash");
